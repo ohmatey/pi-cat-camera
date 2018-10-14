528f8f68b26112c4b6ecba7b41a0b6d156d489e6
Aaron McPherson <aaronmcpherson@hotmail.com>
2018-10-14 14:21:13 +0700

    🤙

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-var express = require('express')
-var app = express()
-var http = require('http').Server(app)
-var io = require('socket.io')(http)
-var fs = require('fs')
-var path = require('path')
+const express = require('express')
+const app = express()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
+const fs = require('fs')
+const path = require('path')
+const kittydar = require('kittydar')
+const { loadImage } = require('canvas')
 
-var spawn = require('child_process').spawn
+const spawn = require('child_process').spawn
 var proc
 
 app.use('/', express.static(path.join(__dirname, 'stream')))
@@ -16,14 +18,14 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html')
 })
 
-var sockets = {}
+const sockets = {}
 
-io.on('connection', function(socket) {
+io.on('connection', socket => {
 
   sockets[socket.id] = socket
   console.log("Total clients connected : ", Object.keys(sockets).length)
 
-  socket.on('disconnect', function() {
+  socket.on('disconnect', () => {
     delete sockets[socket.id]
 
     // no more sockets, kill the stream
@@ -34,17 +36,16 @@ io.on('connection', function(socket) {
     }
   })
 
-  socket.on('start-stream', function() {
+  socket.on('start-stream', () => {
     startStreaming(io)
   })
-
 })
 
 http.listen(3000, function() {
   console.log('listening on *:3000')
 })
 
-function stopStreaming() {
+const stopStreaming () => {
   if (Object.keys(sockets).length == 0) {
     app.set('watchingFile', false)
     if (proc) proc.kill()
@@ -52,21 +53,31 @@ function stopStreaming() {
   }
 }
 
-function startStreaming(io) {
-
+const startStreaming = io => {
   if (app.get('watchingFile')) {
     io.sockets.emit('liveStream', 'image_stream.jpg?_t=' + (Math.random() * 100000))
     return
   }
 
-  var args = ["-w", "640", "-h", "480", "-o", "./public/image_stream.jpg", "-t", "999999999", "-tl", "10"]
+  const args = ["-w", "640", "-h", "480", "-o", "./public/image_stream.jpg", "-t", "999999999", "-tl", "10"]
   proc = spawn('raspistill', args)
 
   console.log('Watching for changes...')
 
   app.set('watchingFile', true)
 
-  fs.watchFile('./public/image_stream.jpg', function(current, previous) {
+  fs.watchFile('./public/image_stream.jpg', (current, previous) => {
+
+    loadImage('./public/image_stream.jpg')
+      .then(img => {
+        console.log(img)
+        const cats = kittydar.detectCats(img);
+
+        console.log("there are", cats.length, "cats in this photo");
+
+        console.log(cats[0]);
+      })
+
     io.sockets.emit('liveStream', 'image_stream.jpg?_t=' + (Math.random() * 100000))
   })
 
