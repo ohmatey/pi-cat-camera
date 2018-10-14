52512e25e6bfe30a74ba0a1f249b2f52ce1a8037
Aaron McPherson <aaronmcpherson@hotmail.com>
2018-10-14 13:01:31 +0700

    ⚡️

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ var proc
 
 app.use('/', express.static(path.join(__dirname, 'stream')))
 
+app.use(express.static('public'))
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html')
@@ -29,7 +30,7 @@ io.on('connection', function(socket) {
     if (Object.keys(sockets).length == 0) {
       app.set('watchingFile', false)
       if (proc) proc.kill()
-      fs.unwatchFile('./image_stream.jpg')
+      fs.unwatchFile('./public/image_stream.jpg')
     }
   })
 
@@ -47,7 +48,7 @@ function stopStreaming() {
   if (Object.keys(sockets).length == 0) {
     app.set('watchingFile', false)
     if (proc) proc.kill()
-    fs.unwatchFile('./image_stream.jpg')
+    fs.unwatchFile('./public/image_stream.jpg')
   }
 }
 
@@ -58,7 +59,7 @@ function startStreaming(io) {
     return
   }
 
-  var args = ["-w", "640", "-h", "480", "-o", "./image_stream.jpg", "-t", "999999999", "-tl", "100"]
+  var args = ["-w", "640", "-h", "480", "-o", "./public/image_stream.jpg", "-t", "999999999", "-tl", "100"]
   proc = spawn('raspistill', args)
 
   console.log('Watching for changes...')
@@ -72,7 +73,7 @@ function startStreaming(io) {
 }
 
 try {
-  var args = ["-w", "640", "-h", "480", "-o", "./image_stream.jpg", "-t", "999999999", "-tl", "100"]
+  var args = ["-w", "640", "-h", "480", "-o", "./public/image_stream.jpg", "-t", "999999999", "-tl", "100"]
   proc = spawn('raspistill', args)
 
   console.log(proc)
