81eb94db4f8b863462b4c1d218615b792841cfc7
Aaron McPherson <aaronmcpherson@hotmail.com>
2018-10-14 12:03:30 +0700

    🎉

---
+++ index.js
@@ -0,0 +1,14 @@
+const PiCamera = require('pi-camera')
+const myCamera = new PiCamera({
+  mode: 'photo',
+  output: `${ __dirname }/test.jpg`,
+  width: 640,
+  height: 480,
+  nopreview: true,
+})
+
+myCamera.snap()
+  .then(result => {
+    console.log('result', result)
+  })
+  .catch(console.log)
