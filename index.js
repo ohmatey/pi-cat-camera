9a3d052883c544321f23fc698c018510c7555890
Aaron McPherson <aaronmcpherson@hotmail.com>
2018-10-14 13:19:59 +0700

    ⚡️

--- index.js
+++ index.js
@@ -71,14 +71,3 @@ function startStreaming(io) {
   })
 
 }
-
-try {
-  var args = ["-w", "640", "-h", "480", "-o", "./public/image_stream.jpg", "-t", "999999999", "-tl", "100"]
-  proc = spawn('raspistill', args)
-
-  console.log(proc)
-} catch (e) {
-  console.log(e)
-} finally {
-
-}
