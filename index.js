e08a25255149b84911a5c22ec97f3c959cec89fa
Aaron McPherson <aaronmcpherson@hotmail.com>
2018-10-14 16:03:27 +0700

    .

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http').Server(app)
 const io = require('socket.io')(http)
 const fs = require('fs')
 const path = require('path')
-const Canvas = require('./kittydar/kittydar/nodule_modules/canvas')
+const Canvas = require('canvas')
 console.log(Canvas)
 const kittydar = require('./kittydar/kittydar')
 
@@ -71,39 +71,39 @@ const startStreaming = io => {
 
     fs.readFile('./public/image_stream.jpg', (err, data) => {
       if (err) {
-      return console.error(err);
-    }
-    let img = new Canvas.Image; // creating an image object
-    img.src = data;
-
-    let w = img.width;
-    let h = img.height;
-
-    let canvas = new Canvas(w, h);
-    let ctx = canvas.getContext('2d');
-    ctx.drawImage(img, 0, 0, w, h, 0, 0, w, h);
-
-    console.log('PID ' + process.pid + ': ditecting cats in the photo...');
-
-    let cats = kittydar.detectCats(canvas);
-    console.log('There are', cats.length, 'cats in this photo');
-
-    let base64Img = '';
-
-    if(cats.length > 0) {
-
-      // Draw a rectangle around the detected cat's face
-      ctx.strokeStyle = 'rgba(255, 64, 129, 0.8)';
-      ctx.lineWidth = 2;
-
-      for (let i = 0; i < cats.length; i++) {
-        let cat = cats[i];
-        console.log(cat);
-        ctx.strokeRect(cat.x, cat.y, cat.width, cat.height);
+        return console.error(err)
       }
 
-      base64Img = canvas.toDataURL(); // png by default. jpeg is currently not supported by node-canvas
-    }
+      let img = new Canvas.Image // creating an image object
+      img.src = data
+
+      let w = img.width
+      let h = img.height
+
+      let canvas = new Canvas(w, h)
+      let ctx = canvas.getContext('2d')
+      ctx.drawImage(img, 0, 0, w, h, 0, 0, w, h)
+
+      console.log('PID ' + process.pid + ': ditecting cats in the photo...')
+      console.log(canvas)
+      let cats = kittydar.detectCats(canvas)
+      console.log('There are', cats.length, 'cats in this photo')
+      console.log(cats)
+      let base64Img = ''
+
+      if(cats.length > 0) {
+        // Draw a rectangle around the detected cat's face
+        ctx.strokeStyle = 'rgba(255, 64, 129, 0.8)'
+        ctx.lineWidth = 2
+
+        for (let i = 0 i < cats.length i++) {
+          let cat = cats[i]
+          console.log(cat)
+          ctx.strokeRect(cat.x, cat.y, cat.width, cat.height)
+        }
+
+        base64Img = canvas.toDataURL() // png by default. jpeg is currently not supported by node-canvas
+      }
     })
 
     io.sockets.emit('liveStream', 'image_stream.jpg?_t=' + (Math.random() * 100000))
